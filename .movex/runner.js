2378daec7820347d54e3e30f4c72593267675a61
Gabriel C. Troia <catalin.troia@gmail.com>
2023-07-01 17:41:58 -0600

    Remove .movex from repo

--- .movex/runner.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const definition = require('./dist');
-const movexServer = require('movex-server');
-movexServer.movexServer({}, definition.default);
-//# sourceMappingURL=runner.js.map
